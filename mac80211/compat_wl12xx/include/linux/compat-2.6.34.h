15dcc8480adefe52d010ebda7eb7892e980cd40d
qingliu <shuiqingliu14@gmail.com>
2013-05-23 01:28:47 +0800

    Update compat-2.6.34.h
    fix compile with jellyx kernel 2.6.32-ion

--- mac80211/compat_wl12xx/include/linux/compat-2.6.34.h
+++ mac80211/compat_wl12xx/include/linux/compat-2.6.34.h
@@ -159,32 +159,7 @@ do {								\
 /* source: include/linux/netdevice.h */
 
 
-static inline void device_lock(struct device *dev)
-{
-#if defined(CONFIG_PREEMPT_RT) || defined(CONFIG_PREEMPT_DESKTOP)
-        mutex_lock(&dev->mutex);
-#else
-	down(&dev->sem);
-#endif
-}
 
-static inline int device_trylock(struct device *dev)
-{
-#if defined(CONFIG_PREEMPT_RT) || defined(CONFIG_PREEMPT_DESKTOP)
-	return mutex_trylock(&dev->mutex);
-#else
-	return down_trylock(&dev->sem);
-#endif
-}
-
-static inline void device_unlock(struct device *dev)
-{
-#if defined(CONFIG_PREEMPT_RT) || defined(CONFIG_PREEMPT_DESKTOP)
-        mutex_unlock(&dev->mutex);
-#else
-	up(&dev->sem);
-#endif
-}
 
 #if defined(CONFIG_PCMCIA) || defined(CONFIG_PCMCIA_MODULE)
 #define PCMCIA_DEVICE_PROD_ID3(v3, vh3) { \
